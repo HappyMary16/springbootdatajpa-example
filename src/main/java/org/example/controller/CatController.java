8ce22e284852c577ab31612abba46fb32c021d91
mborodin <m.borodin.1999@gmail.com>
2025-03-04 21:11:18 +0100

    added logs

--- src/main/java/org/example/controller/CatController.java
+++ src/main/java/org/example/controller/CatController.java
@@ -3,6 +3,8 @@ package org.example.controller;
 import org.example.model.Cat;
 import org.example.model.CatBehaviour;
 import org.example.service.CatService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,6 +19,8 @@ import java.util.Collection;
 @RestController
 public class CatController {
 
+    private static final Logger log = LoggerFactory.getLogger(CatController.class);
+
     private final CatService catService;
 
     public CatController(CatService catService) {
@@ -25,6 +29,7 @@ public class CatController {
 
     @GetMapping
     public Collection<Cat> getAllCats(@RequestParam(required = false) CatBehaviour behaviour) {
+        log.debug("Getting all cats. Behaviour: {}", behaviour);
         if (behaviour != null) {
             return catService.getCatsByBehaviour(behaviour);
         }
@@ -34,39 +39,53 @@ public class CatController {
 
     @GetMapping("/{catName}")
     public Cat getCat(@PathVariable String catName) {
+        log.debug("Getting cat. Cat name: {}", catName);
+
         if (catService.getCat(catName) == null) {
+            log.debug("Cat '{}' was not found.", catName);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
+        log.debug("Cat '{}' was found.", catName);
         return catService.getCat(catName);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Cat createCat(@RequestBody Cat cat) {
+        log.debug("Creating a cat. Cat: {}", cat);
+
         if (catService.addCat(cat)) {
+            log.debug("Cat '{}' was created successfully.", cat.getName());
             return catService.getCat(cat.getName());
         } else {
+            log.debug("Cat '{}' was not created.", cat.getName());
             throw new ResponseStatusException(HttpStatus.CONFLICT);
         }
     }
 
     @PutMapping("/{catName}")
     public Cat updateCat(@PathVariable String catName, @RequestBody Cat cat) {
+        log.debug("Updating a cat. Cat: {}", cat);
         if (catService.getCat(catName) == null) {
+            log.debug("Cat '{}' was not updated as it was not found.", catName);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
+        log.debug("Cat '{}' was updated successfully.", catName);
         return catService.updateCat(cat);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{catName}")
     public Cat deleteCat(@PathVariable String catName) {
+        log.debug("Deleting a cat. Cat: {}", catName);
         if (catService.getCat(catName) == null) {
+            log.debug("Cat '{}' was not deleted as it was not found.", catName);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
+        log.debug("Cat '{}' was deleted successfully.", catName);
         return catService.deleteCat(catName);
     }
 }
--- src/main/java/org/example/service/CatService.java
+++ src/main/java/org/example/service/CatService.java
@@ -1,10 +1,12 @@
 package org.example.service;
 
-import jakarta.annotation.PreDestroy;
 import jakarta.transaction.Transactional;
 import org.example.model.Cat;
 import org.example.model.CatBehaviour;
 import org.example.repository.CatRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -12,12 +14,15 @@ import java.util.*;
 @Service
 public class CatService {
 
+    private static final Logger log = LoggerFactory.getLogger(CatService.class);
+
+    @Value("${app.max-cat-number}")
+    private int maxCatNumber;
+
     private final CatRepository catRepository;
 
     public CatService(CatRepository catRepository) {
         this.catRepository = catRepository;
-        catRepository.save(new Cat("Tomas", CatBehaviour.CALM));
-        catRepository.save(new Cat("Oscar", CatBehaviour.NORMAL));
     }
 
     public Collection<Cat> getAllCats() {
@@ -33,30 +38,34 @@ public class CatService {
     }
 
     public boolean addCat(Cat cat) {
+        log.info("Creating a cat. Cat: {}", cat);
+
+        if (catRepository.count() == maxCatNumber) {
+            log.info("Cat '{}' can not be created. Max supported amount of cats: {}.", cat.getName(), maxCatNumber);
+            return false;
+        }
+
         if (catRepository.existsById(cat.getName())) {
+            log.info("Cat '{}' can not be created. It already exists.", cat.getName());
             return false;
         }
 
         catRepository.save(cat);
+
+        log.info("Cat '{}' is created.", cat.getName());
         return true;
     }
 
     public Cat updateCat(Cat cat) {
+        log.info("Updating a cat. Cat: {}", cat);
         return catRepository.save(cat);
     }
 
     @Transactional
     public Cat deleteCat(String catName) {
+        log.info("Deleting a cat. Cat: {}", catName);
         Cat cat = catRepository.findCat(catName);
         catRepository.deleteCat(catName);
         return cat;
-    }
-
-    /**
-     * Видаляємо всіх котів з Бази Даних коли програма завершується
-     */
-    @PreDestroy
-    public void destroy() {
-        catRepository.deleteAll();
     }
 }
